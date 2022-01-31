de2f8d603a447a0eb775bea8fb13db904422b16e
Felix Sanders <98698093+CEO-of-Stellar-Industries@users.noreply.github.com>
2022-01-31 19:20:18 +0000

    Update Stellar_Website.py

--- Stellar_Website.py
+++ Stellar_Website.py
@@ -2,3 +2,13 @@ from randint import *
 import flask
 # This is a lie, you need to find it!
 print('This Website is out of commision')
+
+def Main_Web():
+  print("Stellar Company's")
+  print('\n\n\n\n\n\n\nStellar Industrties \n\nOmega company')
+
+def Info():
+  print('Please await further updates')
+  print('visit: www.replit.com/StellerStudios')
+  print('\n\nIf you have any questions you can email us at: stellar.company.g127')
+  print('Please dont spam us')
